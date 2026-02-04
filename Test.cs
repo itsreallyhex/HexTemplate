f223912a37ddf80a940ee47f9fd1e14770043e55
Elhexo <a.a.alaniz27@gmail.com>
2026-02-04 16:54:56 +0300

    Made it public

--- Test.cs
+++ Test.cs
@@ -1,22 +1,14 @@
 ﻿using BepInEx;
 using GorillaLocomotion;
-using Photon.Realtime;
+using Photon.Pun;
 using UnityEngine;
+using Photon.Realtime;
+using HarmonyLib;
+using Photon;
+
+// This is a Template I made witouit a menu or anything. I made the injection and the rest is to u!. Enjoy :)  04/02/2026-06/02/2026 - Hex
 
 namespace Hex_Template
 {
-    public class Test : BaseUnityPlugin
-    {
-        public void Update()
-        {
-            if (UnityInput.Current.GetKeyDown(KeyCode.T))
-            {
-                Logger.LogInfo("T key was pressed! Hex is here.");
-            }
-
-
-
-          
-        }
-    }
+   
 }
